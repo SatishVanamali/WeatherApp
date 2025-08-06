8378c1d6fe8892f5a2033a8c65f2aef203bc4d16
SatishVanamali <satishkumarvanamali@gmail.com>
2025-08-07 00:50:16 +0530

    InitialUpdate
    InitialUpdate

---
+++ WeatherApp.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Satish Vanamali on 07/08/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WeatherApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            WeatherLaunchScreen()
+        }
+    }
+}
